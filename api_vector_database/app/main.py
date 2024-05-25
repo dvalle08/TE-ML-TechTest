9c09655739e2060083db53a23687f5bb794b47a9
Diego <valle.diegof@gmail.com>
2024-05-25 16:24:12 -0500

    implementing Chroma Vector DataBase

--- api_vector_database/app/main.py
+++ api_vector_database/app/main.py
@@ -1,6 +1,8 @@
 import fitz  # PyMuPDF  
 import pandas as pd  
-  
+import chromadb
+from chromadb.utils import embedding_functions
+
 def extract_text_by_paragraph(pdf_path):  
     document = fitz.open(pdf_path)  
     data = []  
@@ -10,24 +12,43 @@ def extract_text_by_paragraph(pdf_path):
         #blocks[0][4]
         #blocks[1][4]
         #print('\n')
-    
-        for block in blocks:
+        blocks_len = str(len(blocks))
+        for No, block in enumerate(blocks):
             if block[4].strip():  # Ignorar bloques vacíos  
                 data.append({  
-                    'page': page_num + 1,  
+                    'page': page_num + 1,
+                    'block_no': str(No)+' de '+blocks_len,  
                     'text': block[4].strip()  
                 })  
     return data 
 
 pdf_path = "data/AS-IS-Residential-Contract-for-Sale-And-Purchase (1).pdf"  
 extracted_paragraphs = extract_text_by_paragraph(pdf_path)  
-print(extracted_paragraphs)
-# Crear un DataFrame con el texto de cada párrafo  
+
 df_paragraphs = pd.DataFrame(extracted_paragraphs)  
-  
-# Mostrar el DataFrame  
-print(df_paragraphs)  
+df_paragraphs = df_paragraphs.reset_index().rename(columns={'index':'ids'})
 
+sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name = 'all-MiniLM-L6-v2')
 
+chroma_client = chromadb.Client()
+db = chroma_client.create_collection(name='Residential_Contract', embedding_function=sentence_transformer_ef)
+
+db.add(
+    ids=df_paragraphs['ids'].astype(str).tolist(),
+    documents=df_paragraphs['text'].tolist(),
+    metadatas= df_paragraphs.drop(['ids','text'],axis=1).to_dict('records')
+)
+
+#!----------------------------------------------------------------
+#Queries
+db.peek(3)
+result = db.query(
+    query_texts=['PROPERTY INSPECTIONS AND RIGHT TO CANCEL'],
+    n_results=2
+)
+
+result.keys()
+result['metadatas']
+#!------------------------------------------------------------
 
 
